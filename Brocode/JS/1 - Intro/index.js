4fe1b580276f1fd11df707bbf5682dbfcfca3ce1
Gavin Blackie <blackieg@sheridancollege.ca>
2026-02-22 00:41:48 -0500

    Relearned about JS basics (started tutorial)

---
+++ Brocode/JS/1 - Intro/index.js
@@ -0,0 +1,16 @@
+console.log("Hello spongebob!");
+console.log('Patrick Star');
+console.log('Mr. Krabs');
+
+// Prompting a pop-up alert on screen
+//window.alert("ALERT!!!");
+//window.alert("I like fish. ");
+
+/* Multi line comment for Spongebob to see!!!*/
+
+// Editing elements with JS:
+
+h1 = document.getElementById("myH1");
+h1.textContent = "Hello, Spongebob Squarepants";
+
+document.getElementById("myP").textContent = `Lorem is poop`;
