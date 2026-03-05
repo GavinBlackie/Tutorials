6f3542d26da0b7c6646cc51f29084c0ab67d3db8
Gavin Blackie <blackieg@sheridancollege.ca>
2026-03-05 15:09:17 -0500

    Commiting changed files from Props React Practice

---
+++ Brocode/React/3-props/src/Student.tsx
@@ -0,0 +1,16 @@
+type StudentProp = {
+    name : string,
+    age : number,
+    isStudent : boolean
+};
+
+function Student(props : StudentProp) {
+    return (
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent ? "True" : "False"}</p>
+        </div>
+    )
+}
+export default Student; // Exports whole component file
