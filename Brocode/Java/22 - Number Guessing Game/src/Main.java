0a77ff72177ae61ba05686e61caea401026d83fe
Gavin Blackie <blackieg@sheridancollege.ca>
2026-05-16 17:32:09 -0400

    Successfully added basic replayability in the number guessing game in java!

--- Brocode/Java/22 - Number Guessing Game/src/Main.java
+++ Brocode/Java/22 - Number Guessing Game/src/Main.java
@@ -18,22 +18,50 @@ public class Main {
 		// the tutorial video at all, then I will watch and contrast them to
 		// further my learning!! :))) 
 		
+		boolean isPlayingGame = true;
+		
 		/* Need a random object !!
 		* But you don't need to close it like the scanner,
 		* just let it go out of scope :) */
 		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
 		
-		int min = 0, max = 10;
-		int guess = 0, answer = 5;
-		
-		// Get the guess from the user
-		guess = extractGuess(guess, min, max, scanner);
-		
-		// Check if user has won or not!
-		boolean hasWon = checkWin(guess, answer);
-		
-		
+		while (isPlayingGame) {
+			
+			int min = 0, max = 10;
+			int guess = 0, answer = random.nextInt(min, max);
+			
+			// Get the guess from the user
+			guess = extractGuess(guess, min, max, scanner);
+			
+			// Check if user has won or not!
+			boolean hasWon = checkWin(guess, answer);
+			
+			String endMsg = (hasWon) ? 
+					"You have won! Would you like to play again?(Y/N): " : 
+					"You lost. Would you like to play again?(Y/N): ";
+			System.out.print(endMsg);
+			
+			// Will use a map to decide if the user wants to keep playing or not!!
+			// https://docs.oracle.com/javase/7/docs/api/java/util/HashMap.html
+			final Map<String, Boolean> yesNoMap = new HashMap<String, Boolean>();
+			yesNoMap.put("yes", true);
+			yesNoMap.put("y", true);
+			yesNoMap.put("no", false);
+			yesNoMap.put("n", false);
+			
+			String userInput = scanner.next();
+			userInput = userInput.toLowerCase();
+			userInput = userInput.strip();
+			
+			// Use .get method for java (similar to [] in Python dictionaries)
+			// https://www.geeksforgeeks.org/java/map-get-method-in-java-with-examples/
+			if (yesNoMap.get(userInput) == false) {
+				isPlayingGame = false;
+			}
+			
+			//scanner.nextLine(); // flush just in case
+		}
 		
 		scanner.close();
 	}
