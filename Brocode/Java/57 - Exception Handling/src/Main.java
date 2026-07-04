633f4239beccdee76982c8aa8c150c6547f07a93
Gavin Blackie <blackieg@sheridancollege.ca>
2026-07-03 22:25:46 -0400

    Tried a LeetCode LinkedList question - got very close, just need to find the right conditions for an exceptional case - also started the exception handling lesson for java

---
+++ Brocode/Java/57 - Exception Handling/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// Exception = An event that interrupts the normal flow of a program
+		//	     (Divide by 0, file not found, input mismatch, cast errors)
+		//           Typically surround dangerous code with a try{} block
+		// 	     try{}, catch{}, finally{}
+		//
+		//  	     Catches work in buckets. 
+		//	     DO NOT have more generic exception catches
+		//	     at the top of the try-catch chain.
+		//	     "Catch specific scenarios first"
+
+
+	}
+}
