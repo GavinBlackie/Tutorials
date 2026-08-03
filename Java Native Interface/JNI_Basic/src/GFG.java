8b86264d04e983e32daf432570844390e42b5bfc
Gavin Blackie <blackieg@sheridancollege.ca>
2026-08-03 17:10:07 -0400

    Decided to move the Data Structures & Algorithms tutorial code to its own area (since its under a completely different video than Java)

--- Java Native Interface/JNI_Basic/src/GFG.java
+++ Java Native Interface/JNI_Basic/src/GFG.java
@@ -25,7 +25,6 @@ public class GFG {
 	public native double multiply(double a, double b);
 	
 	public static void main(String[] args) {
-		autoCompileJNICode();
 		
 		System.out.println("Java & C++ JNI Test!");
 		GFG gfg = new GFG();
