dc5cd5203028a6fc69a80cbd526f778634e76a95
Gavin Blackie <blackieg@sheridancollege.ca>
2026-09-05 20:53:53 -0400

    Re-committing first C# project (not used to Visual Studio's git commit UI)

---
+++ Brocode/C#/1. C# Beginner Tutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    // This format is similar to Java!!
+    class Program
+    {
+        // "A magical spell to start the program"
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("I like Pizza. ");
+            Console.Write("I also like watching Spongebob");
+            Console.Write(" and Psycho Pass. ");
+            Console.WriteLine("\nThank you for reading monkey brain!! :)) ");
+
+            Console.WriteLine("\t\t\tBanana");
+            Console.Beep(); // beep beep beep (windows beep sound)
+            Console.Beep();
+            Console.Beep();
+        }
+    }
+}
