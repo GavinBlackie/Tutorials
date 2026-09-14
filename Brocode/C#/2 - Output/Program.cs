0abba83007eba322690cc27871819d0a23f59445
Gavin Blackie <blackieg@sheridancollege.ca>
2026-09-13 22:45:51 -0400

    [C#] Did some casual practice with user input and output

---
+++ Brocode/C#/2 - Output/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _2___Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Apple pear ");
+            Console.WriteLine("banana rama spongebob");
+
+            // This is a comment
+
+            /* This
+             * is
+             * a
+             * multiline
+             * comment
+             * yay!!!
+             */
+
+            Console.WriteLine("Some \t useful \t\t characters\nWOW!!\npatrick star");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Brocode/C#/6 - UserInput/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _6___UserInput
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What's your name?: ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Hello & Welcome, " + name);
+
+            Console.Write("What's your age?: ");
+            bool isPrompting = true;
+            uint age = 0;
+            while (isPrompting)
+            {
+                try
+                {
+                    age = Convert.ToUInt32(Console.ReadLine());
+                    isPrompting = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That's not a valid integer");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("That number is out of the int32 range");
+                }
+            }
+
+            Console.WriteLine("You are " + age + " years old. ");
+
+            Console.ReadKey();
+        }
+    }
+}
